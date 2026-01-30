d974949ea740e4b331d06d9412fc8fea7b7c3b14
ViaLimao <arrosante@gmail.com>
2026-01-29 21:25:22 -0300

    (backend) feat: created DeleteUserByIdUseCase

---
+++ backend/whatsapp/src/main/java/whatsapp/whatsapp/application/user/DeleteUserByIdUseCase.java
@@ -0,0 +1,27 @@
+package whatsapp.whatsapp.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import whatsapp.whatsapp.infra.persistance.user.UserRepository;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteUserByIdUseCase {
+    private final UserRepository repository;
+
+    /*
+    * Aviso: talvez seja interessante adicionar uma exceção p/ o caso de não existir
+    * nenhum usuário cujo ID corresponde ao passado pelo parâmetro.
+    * */
+    public void execute(UUID id) {
+        /*
+        * Regra de Negócio: Pensar nas implicações de se excluir um usuário.
+        * Por exemplo, o que iria acontecer com suas mensagens, ou, ele seria
+        * automáticamente removido dos grupos que fazia parte?
+        * */
+
+        repository.deleteById(id);
+    }
+}
