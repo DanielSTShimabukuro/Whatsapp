9ff63281beceabbcde06f04a64cacf554ec59e2d
ViaLimao <arrosante@gmail.com>
2026-01-29 21:24:58 -0300

    (backend) feat: added findAllByUsernameContaining method in UserRepository so the search by username is more inclusive

--- backend/whatsapp/src/main/java/whatsapp/whatsapp/infra/persistance/user/UserRepository.java
+++ backend/whatsapp/src/main/java/whatsapp/whatsapp/infra/persistance/user/UserRepository.java
@@ -3,10 +3,17 @@ package whatsapp.whatsapp.infra.persistance.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import whatsapp.whatsapp.domain.user.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User>  findByUserTag(String userTag);
     boolean         existsByUserTag(String userTag);
+
+    /*
+    * A diretiva 'Containing' faz com que os resultados desse metodo sejam inclusivos.
+    * Por exemplo, dado um username "João", passar apenas "J" ou "Joã" trará "João" nos resultados.
+    * */
+    List<User>      findAllByUsernameContaining(String username);
 }
