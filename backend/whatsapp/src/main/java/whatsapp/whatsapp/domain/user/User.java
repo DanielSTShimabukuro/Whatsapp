5ffb0c8edbcd1f2b7432daba8546cecc5010460d
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-02-02 19:19:26 -0300

    (backend): add Group Entity

--- backend/whatsapp/src/main/java/whatsapp/whatsapp/domain/user/User.java
+++ backend/whatsapp/src/main/java/whatsapp/whatsapp/domain/user/User.java
@@ -1,19 +1,30 @@
 package whatsapp.whatsapp.domain.user;
 
-import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import whatsapp.whatsapp.domain.message.Message;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import whatsapp.whatsapp.domain.group.Group;
+import whatsapp.whatsapp.domain.message.Message;
 
 @Entity
 @Table(name = "users")
@@ -44,7 +55,10 @@ public class User {
     private UserRole userRole;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "author")
-    private List<Message> messages = new ArrayList<>();
+    private Set<Message> messages = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "members")
+    private Set<Group> groups = new HashSet<>();
 
     @CreationTimestamp
     @Column(updatable = false)
